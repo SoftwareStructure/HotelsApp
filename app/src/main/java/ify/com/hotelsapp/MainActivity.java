9d3de9e6ec134d57da66489d482d051dff8bc89b
Guy fadida <guycs100@gmail.com>
2018-10-28 10:48:46 +0200

    Adding a link from user button to LoginActivity

--- app/src/main/java/ify/com/hotelsapp/MainActivity.java
+++ app/src/main/java/ify/com/hotelsapp/MainActivity.java
@@ -2,12 +2,45 @@ package ify.com.hotelsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button Hotels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        setClickListener();
     }
-}
+
+    private void setClickListener() {
+
+        Hotels.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);            }
+                });
+    }
+
+    private void init() {
+       Hotels = (Button)(findViewById(R.id.Hotels));
+    }
+
+
+
+
+    }
+
+
+
+
+
+
