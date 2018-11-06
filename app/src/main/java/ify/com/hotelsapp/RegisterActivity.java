d6b80319ade741d35ac9a54c42a37974126cbb28
Imanuel Eyal <hodiya_sinvani@walla.co.il>
2018-11-06 16:22:59 +0200

    Connecting the 'create Account' button to firebase

--- app/src/main/java/ify/com/hotelsapp/RegisterActivity.java
+++ app/src/main/java/ify/com/hotelsapp/RegisterActivity.java
@@ -1,18 +1,37 @@
 package ify.com.hotelsapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private static final String TAG = "EmailPassword";
 
     private Button createAccountButton;
     private EditText userEmail,userPassword;
     private TextView alreadyHaveAccountLink;
+
+    private FirebaseAuth mAuth;
+
+   // private ProgressBar
 
 
     @Override
@@ -22,24 +41,51 @@ public class RegisterActivity extends AppCompatActivity {
 
         initializeFields();
 
-
-        alreadyHaveAccountLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendUserToLoginActivity();
-            }
-        });
-
-
     }
 
     private void initializeFields() {
 
-
-        createAccountButton=(Button)findViewById(R.id.register_button);
         userEmail=(EditText)findViewById(R.id.register_email);
         userPassword=(EditText)findViewById(R.id.register_password);
-        alreadyHaveAccountLink=(TextView)findViewById(R.id.already_have_an_account_link);
+
+        findViewById(R.id.already_have_an_account_link).setOnClickListener(this);
+        findViewById(R.id.register_button).setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+
+    private void createNewAccount(String email,String password) {
+
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm())
+            return;
+
+       // show
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign up success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail: success");
+                            Toast.makeText(RegisterActivity.this, "Account created successfully.",Toast.LENGTH_LONG).show();
+
+
+                           // sendUserToLoginActivity();
+                        } else {
+                            // If sign up fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            String message=task.getException().toString();
+                            Toast.makeText(RegisterActivity.this, "Error: "+message,Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    //    hideProgressDialog();
+                    }
+                });
+
     }
 
 
@@ -49,5 +95,42 @@ public class RegisterActivity extends AppCompatActivity {
         startActivity(loginIntent);
     }
 
+    private boolean validateForm() {
 
+        boolean valid = true;
+
+        String email = userEmail.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            userEmail.setError("Required.");
+
+            valid = false;
+        } else {
+            userEmail.setError(null);
+        }
+
+        String password = userPassword.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            userPassword.setError("Required.");
+            valid = false;
+        } else
+            userPassword.setError(null);
+
+
+        return valid;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        int id=v.getId();
+
+        if (id==R.id.already_have_an_account_link)
+            sendUserToLoginActivity();
+        else if(id==R.id.register_button) {
+
+            createNewAccount(userEmail.getText().toString(),userPassword.getText().toString());
+        }
+
+    }
 }
