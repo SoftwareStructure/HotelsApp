e4cc51b94b6dd16a95cec94177f965719ec1d166
Imanuel Eyal <hodiya_sinvani@walla.co.il>
2018-11-18 13:25:52 +0200

    Adding some more changes

--- app/src/main/java/ify/com/hotelsapp/RegisterActivity.java
+++ app/src/main/java/ify/com/hotelsapp/RegisterActivity.java
@@ -17,6 +17,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
