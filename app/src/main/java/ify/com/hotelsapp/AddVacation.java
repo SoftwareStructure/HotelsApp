c76b1eab30c226aee9d92decd0d0ed1a72746b1e
Guy fadida <guycs100@gmail.com>
2018-12-25 16:52:00 +0200

    Creating vacation class and activity

--- app/src/main/java/ify/com/hotelsapp/AddVacation.java
+++ app/src/main/java/ify/com/hotelsapp/AddVacation.java
@@ -2,12 +2,93 @@ package ify.com.hotelsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-public class AddVacation extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class AddVacation extends AppCompatActivity  {
+
+    Button add;
+    EditText country;
+    EditText checkIn;
+    EditText checkOut;
+    EditText Price;
+    EditText HotelName;
+    EditText LocalOrAbroad;
+    DatabaseReference VacationDB;
+    DatabaseReference UserDB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_vacation);
+        init();
+        onCLick();
     }
+    private void init() {
+        this.add= findViewById(R.id.Add);
+        this.country=findViewById(R.id.country);
+        this.checkIn=findViewById(R.id.check_in);
+        this.checkOut=findViewById(R.id.check_out);
+        this.HotelName=findViewById(R.id.Hotel);
+        this.Price=findViewById(R.id.Price);
+        this.LocalOrAbroad=findViewById(R.id.LocalAbroad);
+        VacationDB=FirebaseDatabase.getInstance().getReference("Vacation");
+        UserDB=FirebaseDatabase.getInstance().getReference("UsersId");
+    }
+    private void onCLick() {
+
+        this.add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+        if(
+                validfield(country)&&
+                validfield(checkIn)&&
+                validfield(checkOut)&&
+                validfield(Price)&&
+                validfield(LocalOrAbroad)&&
+                validfield(HotelName)) {
+                Toast.makeText(AddVacation.this,"Vacation added", Toast.LENGTH_LONG).show();
+                String id1 =VacationDB.push().getKey();
+                String id2 =UserDB.push().getKey();
+                Vacation vac=new Vacation("Israel",12-02-13,22-03-12,"KingDavid",2220,0);
+                VacationDB.child(id1).setValue(vac);
+
+
+
+        }
+        else return;
+
+
+
+
+
+
+
+
+            }
+        });
+    }
+    private boolean validfield(EditText a){
+        if(TextUtils.isEmpty(a.getText().toString())){
+            a.setError("Enter please");
+            Toast.makeText(AddVacation.this,"erorrrrrrrrrrrrrrr", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+
+
+
+
+
+
+
 }
--- app/src/main/java/ify/com/hotelsapp/Vacation.java
+++ app/src/main/java/ify/com/hotelsapp/Vacation.java
@@ -1,13 +1,28 @@
 package ify.com.hotelsapp;
 
 
-public class Vacation {
+import java.util.Date;
 
-    double price;
+public class Vacation {
+    final static int LOCAL=0;
+    final static int ABROAD=1;
+    String Counrty;
+    int CheckIn;
+    int CheckOut;
     String HotelName;
-    String checkInDate;
-    String checkOutDate;
-    int NumOfPerson;
+    int Price;
+    int local;
+
+    public Vacation(String counrty, int checkIn, int checkOut, String hotelName, int price,int a) {
+        Counrty = counrty;
+        CheckIn = checkIn;
+        CheckOut = checkOut;
+        HotelName = hotelName;
+        Price = price;
+        local =a;
+    }
+
+
 
 
 
